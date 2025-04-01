95fb6f7b69db3b06d9392c5b4d9df8d56005122d
Theon87 <theonfrancis17@gmail.com>
2025-04-01 14:09:57 -0400

    add id to player 1 and 2 option

---
+++ main.js
@@ -0,0 +1,6 @@
+// select number of players
+// if 1 player selected, play vs the computer
+// if 2 player selected, play vs another human
+// player 1 plays first 
+// when player 1 clicks on a box, an "X appears"
+
