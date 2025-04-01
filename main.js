6137e2591de9d7f10eb65f014396ca20bf05e689
Theon87 <theonfrancis17@gmail.com>
2025-04-01 19:52:41 -0400

    update logic to hide the word 'or'

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ document.getElementById("p1").onclick = hideOptions;
 function hideOptions() {
   document.getElementById("p1").style.display = "none";
   document.getElementById("p2").style.display = "none";
+  document.getElementById("or").style.display = "none";
   document.getElementById("table").style.display = "table";
 }
