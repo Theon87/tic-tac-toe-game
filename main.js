59c5815ff524ca818bff33cf10ae8332cf5a3790
Theon87 <theonfrancis17@gmail.com>
2025-04-06 18:46:39 -0400

    added summary score board to keep track of wins and losses

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const play = document.getElementById("play");
-const table = document.getElementById("table");
+const gameBoard = document.getElementById("gameBoard");
 const resetButton = document.getElementById("reset");
+const endButton = document.getElementById("end");
+
+let summary = document.getElementById("summary");
 let result = document.getElementById("result");
 
 let currentPlayer = ""; // 'X' or 'O'
 let cells = document.querySelectorAll("td");
 let board = ["", "", "", "", "", "", "", "", ""];
+
+resetButton.addEventListener("click", resetGame);
 
 const winningCombos = [
   [0, 1, 2],
@@ -17,6 +22,32 @@ const winningCombos = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+
+let countXWins = 0;
+let countOLosses = 0;
+
+let countOWins = 0;
+let countXLosses = 0;
+
+function updateSummaryTable() {
+  return `
+        <tr>
+          <td></td>
+          <td id="wins">WINS</td>
+          <td id="loses">LOSES</td>
+        </tr>
+        <tr>
+          <td>X</td>
+          <td>${countXWins}</td>
+          <td>${countXLosses}</td>
+        </tr>
+        <tr>
+          <td>O</td>
+          <td>${countOWins}</td>
+          <td>${countOLosses}</td>
+        </tr>
+`;
+}
 
 function checkWin(player) {
   return winningCombos.some((combo) => {
@@ -31,19 +62,39 @@ function checkDraw() {
 function playButton() {
   play.addEventListener("click", function () {
     currentPlayer = "X";
-    table.style.display = "table";
+    gameBoard.style.display = "table";
     reset.style.display = "block";
+    endButton.style.display = "block";
+    summary.style.display = "table";
+    play.style.display = "none";
   });
 }
-
-resetButton.addEventListener("click", resetGame);
 
 function resetGame() {
   board = ["", "", "", "", "", "", "", "", ""];
   cells.forEach((cell) => (cell.innerHTML = ""));
-  resetButton.style.display = "none";
-  result.style.display = "none";
+  // result.innerHTML = "";
   currentPlayer = "X";
+  gameBoard.style.display = "table";
+  reset.style.display = "block";
+  endButton.style.display = "block";
+  summary.style.display = "none";
+  play.style.display = "none";
+}
+
+function playAgain() {
+  board = ["", "", "", "", "", "", "", "", ""];
+  cells.forEach((cell) => (cell.innerHTML = ""));
+  result.innerHTML = "";
+  currentPlayer = "X";
+}
+
+function endGame() {
+  endButton.addEventListener("click", function () {
+    result.innerHTML = "";
+    summary.innerHTML = updateSummaryTable();
+    summary.style.display = "table";
+  });
 }
 
 function setupGameBoard() {
@@ -54,11 +105,18 @@ function setupGameBoard() {
         board[index] = currentPlayer;
 
         if (checkWin(currentPlayer)) {
-          result.innerHTML = currentPlayer + " WINS!";
+          result.innerHTML = `PLAYER "${currentPlayer}" WINS!`;
+          if (currentPlayer === "X") {
+            countXWins++;
+            countOLosses++;
+          } else {
+            countOWins++;
+            countXLosses++;
+          }
         } else if (checkDraw()) {
           result.innerHTML = "DRAW";
         } else {
-          currentPlayer = currentPlayer === "X" ? "O" : "X"; // switch turn
+          currentPlayer = currentPlayer === "X" ? "O" : "X";
         }
 
         console.log(board);
@@ -70,6 +128,7 @@ function setupGameBoard() {
 function playGame() {
   playButton();
   setupGameBoard();
+  endGame();
 }
 
 playGame();
