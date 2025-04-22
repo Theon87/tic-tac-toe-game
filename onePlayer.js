05b3d63817ac4c3c356faa6d8eedb26de57a0718
Theon87 <theonfrancis17@gmail.com>
2025-04-22 19:27:06 -0400

    added a new js file to handle one-player game

---
+++ onePlayer.js
@@ -0,0 +1,6 @@
+const onePlayer = document.getElementById("onePlayer");
+const gameBoard = document.getElementById("gameBoard");
+const resetButton = document.getElementById("reset");
+const endButton = document.getElementById("end");
+const playAgainButton = document.getElementById("playAgain");
+const lightDarkTheme = document.getElementById("lightDarkTheme");
